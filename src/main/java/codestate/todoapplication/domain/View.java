5511edb96df28cd5e4cebe68589fba6300dac084
HongSungHak <101723087+HongSungHak@users.noreply.github.com>
2022-10-21 15:50:25 +0900

    first push

---
+++ src/main/java/codestate/todoapplication/domain/View.java
@@ -0,0 +1,12 @@
+package codestate.todoapplication.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class View {
+    @GetMapping("/")
+    public String todo() {
+        return "To-do-Application !";
+    }
+}
